f6878c9a0dc1b9038d1433eb0798d5de4378d277
Indra Rianto <Indrarianto9@gmail.com>
2018-02-04 20:49:05 +0700

    halaman edit password

---
+++ halaman/footer.php
@@ -0,0 +1,42 @@
+
+    <!-- Jquery Core Js -->
+    <script src="<?=url?>assets/plugins/jquery/jquery.min.js"></script>
+    <!-- Bootstrap Core Js -->
+    <script src="<?=url?>assets/plugins/bootstrap/js/bootstrap.js"></script>
+ <script type="text/javascript">
+        $(function () {
+        $('[data-toggle="tooltip"]').tooltip({
+        container: 'body'
+    });
+    });
+</script>
+    <!-- Select Plugin Js -->
+    <link href="<?=url?>assets/plugins/sweetalert/sweetalert.css" rel="stylesheet" />
+    <link rel="stylesheet" type="text/css" href='<?=url?>assets/css/mprogress.min.css' />
+    <script src="<?=url?>assets/plugins/jquery/jquery.min.js"></script>
+    <script src="<?=url?>assets/plugins/bootstrap/js/bootstrap.js"></script>
+    <script src="<?=url?>assets/plugins/node-waves/waves.js"></script>
+    <script src="<?=url?>assets/plugins/jquery-validation/jquery.validate.js"></script>
+
+    <!-- Custom Js -->
+    <script src='<?=url?>assets/js/mprogress.min.js'></script>
+    <script src="<?=url?>assets/js/admin.js"></script>
+    <script src="<?=url?>assets/js/pages/examples/sign-in.js"></script>
+    <script src="<?=url?>assets/plugins/bootstrap-notify/bootstrap-notify.js"></script>
+    <script src="<?=url?>assets/plugins/sweetalert/sweetalert.min.js"></script>
+    <script src="<?=url?>assets/js/pages/ui/dialogs.js"></script>
+    <script src="<?=url?>assets/plugins/bootstrap-select/js/bootstrap-select.js"></script>
+    <!-- plugin -->
+    <script src="<?=url?>assets/plugins/momentjs/moment.js"></script>
+    <script src="<?=url?>assets/plugins/autosize/autosize.js"></script>
+    <script src="<?=url?>assets/plugins/bootstrap-material-datetimepicker/js/bootstrap-material-datetimepicker.js"></script>
+
+    <script src="<?=url?>assets/js/admin.js"></script>
+    <script src="<?=url?>assets/js/pages/forms/basic-form-elements.js"></script>
+
+    <!-- Demo Js -->
+    <script src="<?=url?>assets/js/demo.js"></script>
+   <script>
+</body>
+
+</html>
